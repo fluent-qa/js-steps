808dc45b14807f6a23144d1a374dc6a60c20fbb4
root <patrickwuke@163.com>
2020-04-19 20:12:05 +0800

    init vue demo

--- examples/vue-demos/vue-demos.md
+++ examples/vue-demos/vue-demos.md
@@ -1,7 +1,50 @@
 # Vue Intro
 
 - Vue Basic Usage
+- Vue basic Component
+- Vue More ....
+## Vue Basic Usage
 
+- Vue instance
+- Data binding
+- Two Way Data binding
+- **v** directive,v-if,v-binding,v-on ......
+
+## Vue Basic Component
+
+- How to add component into Vue
+- Vue component: props and template
+
+## Vue More .....
+
+- Template: text/html attribute/javascript expression
+- directive: parameter, binding parameter,follow example: bind data:**url** to html attribute **href**
+
+```html
+<a v-bind:href="url">...</a>
+<a v-bind:[eventName]="doSomething" >
+<form v-on:submit.prevent="onSubmit">...</form>
+```
+
+```html
+<!-- 完整语法 -->
+<a v-bind:href="url">...</a>
+<!-- 缩写 -->
+<a :href="url">...</a>
+<!-- 动态参数的缩写 (2.6.0+) -->
+<a :[key]="url"> ... </a>
+```
+
+```html
+<!-- 完整语法 -->
+<a v-on:click="doSomething">...</a>
+<!-- 缩写 -->
+<a @click="doSomething">...</a>
+<!-- 动态参数的缩写 (2.6.0+) -->
+<a @[event]="doSomething"> ... </a>
+```
+- computation properties
+- 
 
 ## Vue Intro Reference
 
--- examples/vue-demos/vue-todo/index.js
+++ examples/vue-demos/vue-todo/index.js
@@ -1,24 +1,44 @@
-Vue.component('todo-item',{
-    props: ['todo'],  //something need to bind it
-    template: "<li>ID:{{todo.id}},TEXT: {{todo.text}}</li>"
-})
+
 var app = new Vue({
     el: '#app',
     data: {
-        message: "Hello Vue",
-        seen: true,
-        aMsg: 'Page Loaded '+new Date().toLocaleDateString(),
-        todos: [
-            {id: 1,text:"t1"},
-            {id:2,text:"t2"},
-            {id:3,text:"t3"},
-
-        ]
+        message: "this is message",
+        todos: []
     },
-    methods: {
+    computed: {
         reverseMessage: function(){
             //this is the Vue VM instance
-            this.message = this.message.split('').reverse().join('')
+            return this.message.split('').reverse().join('')
         }
+    },
+    beforeCreated: function(){
+        console.log("this is before created")
+    },
+    created: function(){
+        console.log('data is '+this.todos);
+    },
+    beforeMount: function(){
+        console.log("this is before mount")
+    },
+    mounted: function(){
+        console.log(this.el)
+        console.log("this is mounted ...")
+    },
+    beforeUpdate: function(){
+        console.log("this is before update")
+    },
+    updated: function(){
+        console.log("this is updated")
+    },
+    beforeDestroy: function(){
+        console.log("this is before destroy event")
+    },
+    destroyed: function(){
+        console.log("this is destroyed")
     }
+})
+
+app.$watch('todos',function(newValue,oldValue){
+    console.log("new value:"+newValue);
+    console.log("old value:"+oldValue);
 })
